bb1ee2da3273a31f8e3317889d38aa7924bca3bf
xTooth <jamsen.kim@gmail.com>
2018-11-06 22:07:42 +0200

    fixed indentation on testeroos for task 3 week 2.

--- NhlStatistics1/src/test/java/ohtuesimerkki/Testeroos.java
+++ NhlStatistics1/src/test/java/ohtuesimerkki/Testeroos.java
@@ -13,51 +13,51 @@ import static org.junit.Assert.*;
 
 public class Testeroos {
 
-        Reader readerStub = new Reader() {
+    Reader readerStub = new Reader() {
 
-            public List<Player> getPlayers() {
-                ArrayList<Player> players = new ArrayList<Player>();
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
 
-                players.add(new Player("Semenko", "EDM", 4, 12));
-                players.add(new Player("Lemieux", "PIT", 45, 54));
-                players.add(new Player("Kurri",   "EDM", 37, 53));
-                players.add(new Player("Yzerman", "DET", 42, 56));
-                players.add(new Player("Gretzky", "EDM", 35, 89));
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
 
-                return players;
-            }
-        };
-
-        Statistics stats;
-
-        @Before
-        public void setUp(){
-            // luodaan Statistics-olio joka käyttää "stubia"
-            stats = new Statistics(readerStub);
+            return players;
         }
-        @Test
-        public void nameSearchWorks() {
-            Player found = stats.search("Kurri");
-            assertEquals("Kurri",found.getName());
-            Player Found = stats.search("asdgf");
-            assertEquals(null,Found);
-        }
-        @Test
-        public void TeamSearchWerks() {
+    };
 
-            List<Player> found = stats.team("EDM");
+    Statistics stats;
 
-            assertEquals("Semenko",found.get(0).getName());
-            assertTrue(found.size() == 3);
-        }
-        @Test
-        public void TopScorersWorks(){
-            List<Player> found = stats.topScorers(2);
-
-            assertEquals("Gretzky",found.get(0).getName());
-            assertEquals("Lemieux",found.get(1).getName());
-        }
-
-
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
     }
+    @Test
+    public void nameSearchWorks() {
+        Player found = stats.search("Kurri");
+        assertEquals("Kurri",found.getName());
+        Player Found = stats.search("asdgf");
+        assertEquals(null,Found);
+    }
+    @Test
+    public void TeamSearchWerks() {
+
+        List<Player> found = stats.team("EDM");
+
+        assertEquals("Semenko",found.get(0).getName());
+        assertTrue(found.size() == 3);
+    }
+    @Test
+    public void TopScorersWorks(){
+        List<Player> found = stats.topScorers(2);
+
+        assertEquals("Gretzky",found.get(0).getName());
+        assertEquals("Lemieux",found.get(1).getName());
+    }
+
+
+}
 
